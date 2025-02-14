ff0dd04f502e91ae004e938a793117fb4b81a1db
jerryvien <jerryyee0902@gmail.com>
2025-02-15 00:11:40 +0800

    asd

---
+++ blank.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+<!-- Main section with full-screen background -->
+<main class="main" style="min-height: 100vh; background: url('assets/img/your-background.jpg') center center/cover no-repeat;">
+  <!-- Your content goes here -->
+</main>
+
+<?php include 'footer.php'; ?>
