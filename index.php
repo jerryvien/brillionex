2e0683010b4ac9d17c35a31714ce4e3cb9ee25ea
jerryvien <jerryyee0902@gmail.com>
2025-02-15 00:59:58 +0800

    add in services

--- index.php
+++ index.php
@@ -53,8 +53,7 @@
     </div>
   </div>
 </section>
-
-
+<!-- /Hero Section -->
 <!-- About Section -->
 <section id="about" class="about section">
   <div class="container" data-aos="fade-up" data-aos-delay="100">
@@ -79,102 +78,9 @@
     </div>
   </div>
 </section>
+<!-- /About Section -->
 
-
-  <!-- Clients Section -->
-  <section id="clients" class="clients section">
-    <div class="container" data-aos="fade-up" data-aos-delay="100">
-      <div class="swiper init-swiper">
-        <script type="application/json" class="swiper-config">
-          {
-            "loop": true,
-            "speed": 600,
-            "autoplay": {
-              "delay": 5000
-            },
-            "slidesPerView": "auto",
-            "pagination": {
-              "el": ".swiper-pagination",
-              "type": "bullets",
-              "clickable": true
-            },
-            "breakpoints": {
-              "320": {
-                "slidesPerView": 2,
-                "spaceBetween": 40
-              },
-              "480": {
-                "slidesPerView": 3,
-                "spaceBetween": 60
-              },
-              "640": {
-                "slidesPerView": 4,
-                "spaceBetween": 80
-              },
-              "992": {
-                "slidesPerView": 6,
-                "spaceBetween": 120
-              }
-            }
-          }
-        </script>
-        <div class="swiper-wrapper align-items-center">
-          <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
-          <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
-        </div>
-        <div class="swiper-pagination"></div>
-      </div>
-    </div>
-  </section>
-  <!-- /Clients Section -->
-
-  <!-- Features Section -->
-  <section id="features" class="features section">
-    <div class="container">
-      <div class="row gy-4">
-        <div class="features-image col-lg-6" data-aos="fade-up" data-aos-delay="100"><img src="assets/img/features-bg.jpg" alt=""></div>
-        <div class="col-lg-6">
-          <div class="features-item d-flex ps-0 ps-lg-3 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="200">
-            <i class="bi bi-archive flex-shrink-0"></i>
-            <div>
-              <h4>Est labore ad</h4>
-              <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
-            </div>
-          </div>
-          <div class="features-item d-flex mt-5 ps-0 ps-lg-3" data-aos="fade-up" data-aos-delay="300">
-            <i class="bi bi-basket flex-shrink-0"></i>
-            <div>
-              <h4>Harum esse qui</h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-            </div>
-          </div>
-          <div class="features-item d-flex mt-5 ps-0 ps-lg-3" data-aos="fade-up" data-aos-delay="400">
-            <i class="bi bi-broadcast flex-shrink-0"></i>
-            <div>
-              <h4>Aut occaecati</h4>
-              <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-            </div>
-          </div>
-          <div class="features-item d-flex mt-5 ps-0 ps-lg-3" data-aos="fade-up" data-aos-delay="500">
-            <i class="bi bi-camera-reels flex-shrink-0"></i>
-            <div>
-              <h4>Beatae veritatis</h4>
-              <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- /Features Section -->
-
-  <!-- Services Section -->
+<!-- Services Section -->
   <section id="services" class="services section">
     <div class="container section-title" data-aos="fade-up">
       <h2>Services</h2>
@@ -182,78 +88,72 @@
     </div>
     <div class="container">
       <div class="row gy-4">
+        <!-- Service 1: Personal Finance -->
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
           <div class="service-item position-relative">
             <div class="icon">
-              <i class="bi bi-activity"></i>
+              <i class="bi bi-currency-dollar"></i>
             </div>
             <a href="service-details.php" class="stretched-link">
-              <h3>Nesciunt Mete</h3>
+              <h3>Personal Finance</h3>
             </a>
-            <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
+            <p>Customized strategies to manage your personal wealth effectively.</p>
           </div>
         </div>
+        <!-- Service 2: Debt Consolidation -->
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
           <div class="service-item position-relative">
             <div class="icon">
-              <i class="bi bi-broadcast"></i>
+              <i class="bi bi-cash-stack"></i>
             </div>
             <a href="service-details.php" class="stretched-link">
-              <h3>Eosle Commodi</h3>
+              <h3>Debt Consolidation</h3>
             </a>
-            <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+            <p>Streamline your liabilities with a unified debt solution that reduces interest.</p>
           </div>
         </div>
+        <!-- Service 3: Business Finance -->
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
           <div class="service-item position-relative">
             <div class="icon">
-              <i class="bi bi-easel"></i>
+              <i class="bi bi-briefcase"></i>
             </div>
             <a href="service-details.php" class="stretched-link">
-              <h3>Ledo Markt</h3>
+              <h3>Business Finance</h3>
             </a>
-            <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+            <p>Empower your enterprise with strategic funding and growth solutions.</p>
           </div>
         </div>
+        <!-- Service 4: Mortgage Refinance -->
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
           <div class="service-item position-relative">
             <div class="icon">
-              <i class="bi bi-bounding-box-circles"></i>
+              <i class="ri-home-4-line"></i>
             </div>
             <a href="service-details.php" class="stretched-link">
-              <h3>Asperiores Commodit</h3>
+              <h3>Mortgage Refinance</h3>
             </a>
-            <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+            <p>Unlock your property's potential with competitive refinancing options.</p>
           </div>
         </div>
+        <!-- Service 5: Loan Specialist -->
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
           <div class="service-item position-relative">
             <div class="icon">
-              <i class="bi bi-calendar4-week"></i>
+              <i class="bi bi-file-earmark-check"></i>
             </div>
             <a href="service-details.php" class="stretched-link">
-              <h3>Velit Doloremque</h3>
+              <h3>Loan Specialist</h3>
             </a>
-            <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
-          <div class="service-item position-relative">
-            <div class="icon">
-              <i class="bi bi-chat-square-text"></i>
-            </div>
-            <a href="service-details.php" class="stretched-link">
-              <h3>Dolori Architecto</h3>
-            </a>
-            <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+            <p>Expert guidance to secure loan options tailored to your unique needs.</p>
           </div>
         </div>
       </div>
     </div>
   </section>
-  <!-- /Services Section -->
+<!-- /Services Section -->
 
-  <!-- Call To Action Section -->
+<!-- Call To Action Section -->
   <section id="call-to-action" class="call-to-action section dark-background">
     <img src="assets/img/cta-bg.jpg" alt="">
     <div class="container">
